67988ff37f63f6d0c935b512693cffcbd7d2f700
GiangVT <giangvt@beetsoft.com.vn>
2016-10-27 17:20:35 +0700

    fix

--- Example/Pods/Target Support Files/Pods-BEFoldMenuViewController_Tests/Pods-BEFoldMenuViewController_Tests-umbrella.h
+++
@@ -1,6 +0,0 @@
-#import <UIKit/UIKit.h>
-
-
-FOUNDATION_EXPORT double Pods_BEFoldMenuViewController_TestsVersionNumber;
-FOUNDATION_EXPORT const unsigned char Pods_BEFoldMenuViewController_TestsVersionString[];
-
